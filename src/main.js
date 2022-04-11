79565300eeffd3707b688d18da3c5d2cdc02e5ae
xufei <xufei1@accesscorporate.com.cn>
2022-04-11 14:57:57 +0800

    安装vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-08 14:01:05
- * @LastEditTime: 2022-04-11 10:57:57
+ * @LastEditTime: 2022-04-11 14:57:37
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /yeb/src/main.js
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-08 14:01:05
- * @LastEditTime: 2022-04-11 10:39:11
+ * @LastEditTime: 2022-04-11 14:54:47
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /yeb/src/store/index.js
@@ -12,9 +12,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  // 全局对象,保存所有组件公共数据
+  state: {
+    routes:[]
+  },
   getters: {},
-  mutations: {},
+  // 可以改变state值的方法
+  mutations: {
+    initRoutes(state,data){
+      state.routes =  data
+    }
+  },
+
   actions: {},
   modules: {},
 });
