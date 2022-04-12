947654ef8e483a52b679e93de3a076f151ba83c4
xufei <xufei1@accesscorporate.com.cn>
2022-04-12 15:35:20 +0800

    系统标题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <!--
  * @Author: your name
  * @Date: 2022-04-09 15:08:06
- * @LastEditTime: 2022-04-12 14:47:39
+ * @LastEditTime: 2022-04-12 15:34:31
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /yeb/src/views/Home.vue
 -->
 <template>
   <div>
-    <el-container>
-      <el-header>Header</el-header>
+    <el-container >
+      <el-header  class="homeHeader">
+          <div  class="title" >
+              云E办
+          </div>
+      </el-header>
       <el-container>
         <el-aside width="200px">
           <el-menu router unique-opened>
@@ -48,4 +52,47 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+  .homeHeader {
+    background-color: #409eff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 15px;
+    box-sizing: border-box;
+  }
+
+  .homeHeader .title {
+    font-size: 30px;
+    font-family: 华文行楷;
+    color: #ffffff;
+  }
+
+  .homeHeader .userInfo {
+    cursor: pointer;
+  }
+
+  .homeWelcome {
+    text-align: center;
+    font-size: 30px;
+    font-family: 华文行楷;
+    color: #409eff;
+    padding-top: 50px;
+  }
+
+  .homeRouterView {
+    margin-top: 10px;
+  }
+
+  .el-dropdown-link img {
+    width: 48px;
+    height: 48px;
+    border-radius: 24px;
+    margin-left: 8px;
+  }
+
+  .el-dropdown-link {
+    display: flex;
+    align-items: center;
+  }
+</style>
